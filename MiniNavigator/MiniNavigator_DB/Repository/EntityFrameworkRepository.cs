eb0f3415a7bded6411ba35c24a2f50ff983f20a6
ValeryKuzh <valery.kuzh@gmail.com>
2025-12-01 02:19:07 +0300

    Add EFRepository

---
+++ MiniNavigator/MiniNavigator_DB/Repository/EntityFrameworkRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniNavigator_DB.Repository
+{
+    public class EntityFrameworkRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public EntityFrameworkRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id) ?? null;
+        }
+
+        public IQueryable<T> Query()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- MiniNavigator/MiniNavigator_DB/Repository/IRepository.cs
+++ MiniNavigator/MiniNavigator_DB/Repository/IRepository.cs
@@ -8,16 +8,9 @@ namespace MiniNavigator_DB.Repository
 {
     public interface IRepository<T> where T : class
     {
-        T GetById(Guid ID);
-        IEnumerable<T> GetAll();
-        IEnumerable<T> FindAll(Func<T, bool> predicate);
-        void Add(T entity);
-        void Update(T entity);
-        void Delete(T entity);
-        bool Exists(T entity);
+        IQueryable<T> Query();
         Task<T> GetByIdAsync(Guid ID);
         Task<IEnumerable<T>> GetAllAsync();
-        Task<IEnumerable<T>> FindAllAsync(Func<T, bool> predicate);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);

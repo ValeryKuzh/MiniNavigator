a96bd424ebd6b7b0137ed9df78764612fbf73bcb
ValeryKuzh <valery.kuzh@gmail.com>
2025-12-01 01:55:53 +0300

    Add IRepository for db

---
+++ MiniNavigator/MiniNavigator_DB/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniNavigator_DB.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(Guid ID);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> FindAll(Func<T, bool> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        bool Exists(T entity);
+        Task<T> GetByIdAsync(Guid ID);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAllAsync(Func<T, bool> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}

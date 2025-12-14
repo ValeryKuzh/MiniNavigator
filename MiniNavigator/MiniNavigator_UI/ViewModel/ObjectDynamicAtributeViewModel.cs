917f2ab7d1e92a693dea992e768332e03a54f2b0
ValeryKuzh <valery.kuzh@gmail.com>
2025-12-14 18:28:35 +0300

    refactor UI and add data structure for gridView

---
+++ MiniNavigator/MiniNavigator_UI/ViewModel/ObjectDynamicAtributeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniNavigator_UI.ViewModel
+{
+    public class ObjectDynamicAtributeViewModel
+    {
+        public Guid ID { get; set; }
+
+        Dictionary<string, string> Attributes { get; set; } = new Dictionary<string, string>();
+    }
+}
